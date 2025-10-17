4eeea33ba0c79c8fd4ad499700ad87b6f4d146e2
Abishaa <abiramdil@gmail.com>
2025-10-17 14:01:30 +0530

    Changes..

--- Placement/src/main/java/com/tnsif/Placement/repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.tnsif.Placement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.tnsif.Placement.entity.Admin;
-
-public interface AdminRepository extends JpaRepository<Admin, Long>
-{
-    Admin findByUsername(String username); // custom query
-}
